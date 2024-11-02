6c51071c7561d90f765b84961035fddfb420a9e6
Jake Williamson <williamsonj0403@gmail.com>
2024-11-01 20:59:54 -0400

    attempt to re-run program if input is out of bounds as: negative rate, less than 2 for population, or less than 1 for total days. loop to show pop. increase for each day based on previous day's pop.

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -5,8 +5,26 @@
 
 #include <iostream>
 using namespace std;
+int days;
+double pop, rate, total;
+char redo;
 
 int main()
-{
-    cout << "Hello World!\n";
+{ //get data for current population, rate of growth, duration of increase
+    do {
+        cout << "What is the population? ", cin >> pop;
+        cout << "What is the avg. daily population increase? (ex. input 90 if 90%) ";
+        cin >> rate, rate /= 100; //convert percentage to decimal for calc.
+        cout << "For how many days will the population grow? ", cin >> days;
+        if (pop < 2 || days < 1 || rate < 0) //input validation
+            cout << "Invalid input. Try again? Y/N ", cin >> redo;
+        else {
+            for (int counter = 1; counter <= days; counter++) { //show data for each day
+                total = rate * pop;
+                pop += total;
+                cout << "After " << counter << " day(s), the population is " << pop << endl;
+            }
+            cout << "\nDo you want to run the program again? Y/N ", cin >> redo;
+        }
+    } while (redo == 'Y' || redo == 'y');
 }
